863aa9b289f1d07c5727b3ea0602849b5fde01b9
Leandro Amaral <le.amaral@outlook.com>
2026-02-11 15:19:06 -0400

    refactor: enhance UserController tests by integrating jest-mock-extended for improved mocking

--- apps/api/src/users/interfaces/http/users.controller.spec.ts
+++ apps/api/src/users/interfaces/http/users.controller.spec.ts
@@ -4,46 +4,64 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { IUserRepository } from "src/users/domain/repositories/user.repository";
 import request from 'supertest';
 import { UsersModule } from "src/users/users.module";
+import { MockProxy, mock } from "jest-mock-extended";
 
 describe('UserController (Integration)', () => {
     let app: INestApplication;
-    let userRepository: MemoryDBRepository;
+    let userRepository: MockProxy<IUserRepository>;
 
     beforeAll(async () => {
+        userRepository = mock<IUserRepository>();
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [UsersModule]
         })
         .overrideProvider(IUserRepository)
-        .useClass(MemoryDBRepository)
+        .useValue(userRepository)
         .compile()
 
         app = moduleFixture.createNestApplication();
         await app.init();
+    });
 
-        userRepository = moduleFixture.get<MemoryDBRepository>(IUserRepository);
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
 
     it('/users (POST) - should create a user', async () => {
-        const payload = { name: 'Leandro Amaral', email: 'leandro.amaral@conecthus.org.br' }
+        const payload = { name: 'Leandro Amaral', email: 'leandro@email.org.br' }
+        // userRepository.create.mockResolvedValue({
+        //     id: 'uuid-123',
+        //     ...payload
+        // });
+
         const response = await  request(app.getHttpServer())
             .post('/users')
             .send(payload)
         
         expect(response.status).toBe(201)
-        expect(response.body).toHaveProperty('id')
-        expect(userRepository.items.length).toBe(1)
+        expect(response.body).toEqual(expect.objectContaining({
+            id: expect.any(String)
+        }));
     })
 
     it('/users (POST) - should return 400 if email is invalid', async () => {
-    const response = await (request as any)(app.getHttpServer())
-        .post('/users')
-        .send({
-        name: 'Jo',
-        email: 'email-invalido',
-        });
+        const payload = { name: 'John', email: 'email-invalido' }
+        const response = await request(app.getHttpServer())
+            .post('/users')
+            .send(payload);
 
         expect(response.status).toBe(400);
-        expect(response.body.message).toBeDefined(); 
+        expect(response.body).toEqual(
+        expect.objectContaining({
+            message: "Validation failed",
+            errors: expect.arrayContaining([
+                expect.objectContaining({
+                        path: ['email'],
+                        message: "E-mail inválido"
+                    })
+                ])
+            })
+        );
     });
 
     afterAll(async () => {
